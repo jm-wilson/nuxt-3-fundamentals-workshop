0653d943c9a3f4923608f324cdc57c5229de024b
Joe Wilson <joe.wilson@jwils.xyz>
2025-01-04 18:37:27 -0500

    Exercise: Add Photos

--- app/app.vue
+++ app/app.vue
@@ -7,9 +7,19 @@ type Todo = {
   title: string;
   completed: boolean;
 };
+
+type Photo = {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+};
+
 export default defineNuxtComponent({
   data: () => ({
     todoList: [] as Todo[],
+    photoList: [] as Photo[],
   }),
   computed: {
     numTodoItems() {
@@ -21,11 +31,28 @@ export default defineNuxtComponent({
     percentageCompletedTodos() {
       return Math.round((this.numCompletedTodos / this.numTodoItems) * 100);
     },
+    numPhotos() {
+      return this.photoList.length;
+    },
+    numOddAlbums() {
+      return this.photoList.filter((photo) => photo.albumId % 2 !== 0).length;
+    },
+    numEvenAlbums() {
+      return this.photoList.filter((photo) => photo.albumId % 2 === 0).length;
+    },
+    percentageEvenAlbums() {
+      return Math.round((this.numEvenAlbums / this.numPhotos) * 100);
+    },
   },
   methods: {
     fetchTodoList() {
       fetch('https://jsonplaceholder.typicode.com/todos/').then(async (response) => {
         this.todoList = await response.json();
+      });
+    },
+    fetchPhotoList() {
+      fetch('https://jsonplaceholder.typicode.com/photos/').then(async (response) => {
+        this.photoList = await response.json();
       });
     },
   },
@@ -49,25 +76,63 @@ export default defineNuxtComponent({
       >
     </p>
     <h1>Hello Nuxt</h1>
-    <button @click="fetchTodoList">Fetch Todos</button>
     <details>
-      <summary>All todos</summary>
-      <pre>{{ todoList }}</pre>
+      <summary>TODOS</summary>
+      <button @click="fetchTodoList">Fetch Todos</button>
+      <details>
+        <summary>All todos</summary>
+        <pre>{{ todoList }}</pre>
+      </details>
+
+      <p v-if="numCompletedTodos">
+        {{ numCompletedTodos }} / {{ numTodoItems }} todos completed ({{
+          percentageCompletedTodos
+        }}%)
+      </p>
+      <ul>
+        <li v-for="todo in todoList" :key="todo.id">
+          <input disabled type="checkbox" :checked="todo.completed" /> {{ todo.title }}
+        </li>
+      </ul>
     </details>
 
-    <p v-if="numCompletedTodos">
-      {{ numCompletedTodos }} / {{ numTodoItems }} todos completed ({{ percentageCompletedTodos }}%)
-    </p>
-    <ul>
-      <li v-for="todo in todoList" :key="todo.id">
-        <input disabled type="checkbox" :checked="todo.completed" /> {{ todo.title }}
-      </li>
-    </ul>
+    <details>
+      <summary>PHOTOS</summary>
+      <button @click="fetchPhotoList">Fetch photos</button>
+      <p v-if="numPhotos">
+        {{ numPhotos }} photos total ({{ numOddAlbums }} odd, {{ numEvenAlbums }} even ({{
+          percentageEvenAlbums
+        }}%))
+      </p>
+      <ul class="photos">
+        <li v-for="photo in photoList.slice(0, 10)" :key="photo.id">
+          <img :src="photo.thumbnailUrl" :alt="photo.title" />
+        </li>
+      </ul>
+    </details>
   </div>
 </template>
 
 <style>
 :root {
   color-scheme: light dark;
+}
+</style>
+
+<style scoped>
+summary {
+  border: 1px solid #333;
+  padding: 0.5em;
+  margin: 0.5em;
+  display: inline-block;
+}
+
+.photos {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+
+  li {
+    list-style: none;
+  }
 }
 </style>
