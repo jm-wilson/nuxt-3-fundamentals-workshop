d0395d50ab5dff5b6ce30f58040a51086e316d21
Joe Wilson <joe.wilson@jwils.xyz>
2025-01-31 19:16:54 -0500

    Update `app.vue` to use `NuxtPage` and `TheNavbar`, Move "Hello Nuxt" hero to index page

---
+++ app/pages/index.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="columns section">
+    <div class="column">
+      <img src="/todo.jpg" alt="An empty notebook on a desk" />
+      <p>
+        Photo by
+        <a
+          href="https://unsplash.com/@jeshoots?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+          >JESHOOTS.COM</a
+        >
+        on
+        <a
+          href="https://unsplash.com/photos/an-open-empty-notebook-on-a-white-desk-next-to-an-iphone-and-a-macbook-pUAM5hPaCRI?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+          >Unsplash</a
+        >
+      </p>
+    </div>
+    <div class="column">
+      <h1 class="is-size-1">Hello Nuxt</h1>
+    </div>
+  </div>
+</template>
