01d16228f7de18f93a3a5852a087aa4b37ea43de
Joe Wilson <joe.wilson@jwils.xyz>
2025-01-31 20:42:25 -0500

    Add display layout and use it on display index page

--- app/pages/display/index.vue
+++ app/pages/display/index.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="container">
-    <div class="section">
-      <h1 class="is-size-1">Display</h1>
+  <NuxtLayout name="display">
+    <div class="container">
+      <div class="section">
+        <h1 class="is-size-1">Display</h1>
+      </div>
     </div>
-  </div>
+  </NuxtLayout>
 </template>
